385a563c926f50a4f6d4bf16c25057e8eec1a90b
shota saito <s.h4evol@gmail.com>
2013-01-09 09:44:58 +0900

    応用プログラミングA の課題1
    最初のコミット

---
+++ CL_K01_1/CL_K01_1.cpp
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+/*作成者：1T5(22)　斉藤翔太*/
+/*課題名：課題01_1*/
+
+void main(void) {
+	int num1,num2,sum;
+	num1 = 15;
+	num2 = 37;
+	sum = num1+num2;
+	printf("%dに%dを足した値は%dです。\n",num1,num2,sum);
+}
+
